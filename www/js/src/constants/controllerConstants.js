c93d20119b5e50228e2dfc5d6876c0ecd22ddf31
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2015-03-22 12:31:01 +0530

    Removed unused content
    Removed unused content

--- www/js/src/constants/controllerConstants.js
+++ www/js/src/constants/controllerConstants.js
@@ -1,18 +1,5 @@
 ﻿define([],
     function () {
         return {
-            "prices": [
-                    { Value: '1-500', Name: 'Rs 1 - 500' },
-                    { Value: '501-1000', Name: 'Rs 501 - 1000' },
-                    { Value: '1001-2000', Name: 'Rs 1001 - 2000' },
-                    { Value: '2001-3000', Name: 'Rs 2001 - 3000' },
-                    { Value: '3001-5000', Name: 'Rs 3001 - 5000' }
-            ],
-            "sortKeys": [
-                    { Value: 'Title', Name: 'Title' },
-                    { Value: 'Reviews', Name: 'Reviews' },
-                    { Value: 'HighLow', Name: 'Price--High-low' },
-                    { Value: 'LowHigh', Name: 'Price--low-high' }
-            ]
         };
     });
