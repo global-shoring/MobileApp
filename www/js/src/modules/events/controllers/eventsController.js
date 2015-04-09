54f07d51c691427cd82729f2cd4a7b53d9ca4ff3
JanardhanRavada <janardhan.ravada@gmail.com>
2015-04-09 15:47:54 +0530

    event details

--- www/js/src/modules/events/controllers/eventsController.js
+++ www/js/src/modules/events/controllers/eventsController.js
@@ -7,36 +7,22 @@
 
 define([], function () {
     var eventsController = function ($scope, $state , $location) {
-        $scope.collapse = false;
-        $scope.singleEvent="";
         $scope.events = [{
             "name": "Company day",
             "registration": {"startDate": "02-04-2015", "endDate": "03-04-2015"},
-            "eventDate": "10-04-2015"
+            "eventDate": "10-04-2015",
+            "active":false
         },
             {
                 "name": "Family day",
                 "registration": {"startDate": "02-12-2015", "endDate": "03-12-2015"},
-                "eventDate": "12-12-2015"
+                "eventDate": "12-12-2015",
+                "active":false
             }];
 
-        $scope.expand= function(eventName){
-            $scope.collapse = !$scope.collapse;
-            $scope.events.forEach(function (event) {
-                alert("from loop "+event.name);
-                alert("from page " + eventName);
-                alert(eventName == event.name);
-                if(eventName = event.name){
-                    $scope.singleEvent = event;
-                    alert($scope.singleEvent.endDate);
-                   /* $scope.singleEvent.startDate = event.registration.startDate;
-                    alert("from loop "+event.registration.startDate);
-                    alert("start "+singleEvent.startDate);
-                    $scope.singleEvent.endDate = event.registration.endDate;
-                    $scope.singleEvent.eventDate = event.eventDate;*/
-                }
-            })
-        }
+        $scope.toggleEvents = function(event) {
+            event.active = !event.active;
+        };
     };
 
 
--- www/js/src/modules/events/eventsModule.js
+++ www/js/src/modules/events/eventsModule.js
@@ -2,11 +2,9 @@
  * Created by Janardhan on 03-04-2015.
  */
 define(['angular',
-        'src/modules/events/controllers/eventsController',
-    'src/modules/events/controllers/eventDetailsController'],
+        'src/modules/events/controllers/eventsController'],
     function(angular, eventsController, eventDetailsController){
 
         return angular.module('adp.mobile.eventsModule',[])
-            .controller('adp.mobile.controllers.eventsController',['$scope', '$state','$location', eventsController]).
-            controller('adp.mobile.controllers.eventDetailsController', eventDetailsController);
+            .controller('adp.mobile.controllers.eventsController',['$scope', '$state','$location', eventsController]);
     });
